8a4c5c12fb2457fdce255cc7cb2478732debd3a2
Simon Marquis <contact@simon-marquis.fr>
2015-07-05 18:24:19 +0200

    First working implementation. Detect and dump intent.

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Android-UrlScheme
 Test and debug URL scheme on Android
+
+### Known issues
+
+ - UserInfo contained in Uri is Android specific (user ids vs web credentials)
+
+### Test
+
+#### adb
+```shell
+adb shell am start -a android.intent.action.VIEW -d 'test://path/host?param1=value1#hash' -e key1 value1
+```
+
+### References
+
+- https://developer.chrome.com/multidevice/android/intents
+- https://paul.kinlan.me/deep-app-linking-on-android-and-chrome/
+- http://solvedstack.com/questions/how-to-fall-back-to-marketplace-when-android-custom-url-scheme-not-handled
+- http://fokkezb.nl/2013/08/26/url-schemes-for-ios-and-android-1/
+- http://fokkezb.nl/2013/09/20/url-schemes-for-ios-and-android-2/
---
+++ app/src/main/java/fr/smarquis/urlscheme/MainActivity.java
@@ -0,0 +1,108 @@
+package fr.smarquis.urlscheme;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = getIntent();
+        fillIntent(intent, ((TextView) findViewById(R.id.intent)));
+        fillData(intent, ((TextView) findViewById(R.id.data)));
+        fillExtras(intent, ((TextView) findViewById(R.id.extras)));
+    }
+
+
+    private void fillIntent(@Nullable Intent intent, @NonNull TextView textView) {
+        if (intent == null) {
+            textView.setText(Printer.EMPTY);
+            return;
+        }
+        Truss truss = new Truss();
+        Printer.append(truss, "action", intent.getAction());
+        Printer.append(truss, "categories", intent.getCategories());
+        Printer.append(truss, "type", intent.getType());
+        Printer.append(truss, "flags", Flags.decode(intent.getFlags()));
+        Printer.append(truss, "package", intent.getPackage());
+        Printer.append(truss, "component", intent.getComponent());
+        Printer.append(truss, "referrer", getReferrer());
+        textView.setText(truss.build());
+    }
+
+    private void fillData(@Nullable Intent intent, @NonNull TextView textView) {
+        Uri data = intent == null ? null : intent.getData();
+        if (data == null) {
+            textView.setText(Printer.EMPTY);
+            return;
+        }
+
+        Truss truss = new Truss();
+        Printer.append(truss, "raw", data);
+        Printer.append(truss, "scheme", data.getScheme());
+        Printer.append(truss, "host", data.getHost());
+        Printer.append(truss, "port", data.getPort());
+        Printer.append(truss, "path", data.getPath());
+        Printer.appendKey(truss, "query");
+        if (data.isHierarchical()) {
+            for (String queryParameterName : data.getQueryParameterNames()) {
+                Printer.appendSecondary(truss, queryParameterName, data.getQueryParameter(queryParameterName));
+            }
+        }
+        Printer.append(truss, "fragment", data.getFragment());
+        textView.setText(truss.build());
+    }
+
+    private static void fillExtras(@Nullable Intent intent, @NonNull TextView textView) {
+        Bundle extras = intent == null ? null : intent.getExtras();
+        if (extras == null) {
+            textView.setText(Printer.EMPTY);
+            return;
+        }
+
+        Truss truss = new Truss();
+        for (String key : extras.keySet()) {
+            Printer.appendWithClass(truss, key, extras.get(key));
+        }
+        textView.setText(truss.build());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_forward:
+                startActivity(Intent.createChooser(new Intent(getIntent()).setComponent(null), null));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void startIntent(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("test://example.com:8042/over/there/index.dtb?type=animal&name=narwhal#nose"));
+        intent.putExtra("key1", "value1");
+        intent.putExtra("key2", 123456);
+        intent.putExtra("key3", 123456.45);
+        intent.putExtra("key4", true);
+        intent.putExtra("key5", new String[]{"a", "b", "c"});
+        startActivity(intent);
+    }
+}
