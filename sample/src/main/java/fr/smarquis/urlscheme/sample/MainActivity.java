adb928c15e5d6393addf66f9c32291eec29d4cd8
Simon Marquis <contact@simon-marquis.fr>
2015-08-23 18:50:07 +0200

    Gradle update, create library, sample depends on library

---
+++ sample/src/main/java/fr/smarquis/urlscheme/sample/MainActivity.java
@@ -0,0 +1,23 @@
+package fr.smarquis.urlscheme.sample;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("test://lol")));
+            }
+        });
+    }
+
+}
