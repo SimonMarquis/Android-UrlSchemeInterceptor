e6f0d63e01dd5fde80ce225935fd9f895f84ec3c
Simon Marquis <contact@simon-marquis.fr>
2015-10-11 14:54:14 +0200

    Prepare for a local distribution of aar

--- usi/src/main/java/fr/smarquis/usi/InterceptorActivity.java
+++ usi/src/main/java/fr/smarquis/usi/InterceptorActivity.java
@@ -6,6 +6,7 @@ import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.Menu;
@@ -89,21 +90,27 @@ public class InterceptorActivity extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         final Intent nextIntent = getNextIntent();
         if (nextIntent != null) {
-            menu.add(R.string.usi_menu_restart).setIcon(R.drawable.usi_ic_repeat_white_36dp).setShowAsActionFlags(MenuItem.SHOW_AS_ACTION_IF_ROOM).setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            MenuItem restartItem = menu.add(R.string.usi_menu_restart).setIcon(R.drawable.usi_ic_repeat_white_36dp).setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
                 @Override
                 public boolean onMenuItemClick(MenuItem item) {
                     startActivity(Intent.createChooser(nextIntent, null));
                     return true;
                 }
             });
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+                restartItem.setShowAsActionFlags(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+            }
         }
-        menu.add(R.string.usi_menu_settings).setShowAsActionFlags(MenuItem.SHOW_AS_ACTION_NEVER).setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+        MenuItem settingsItem = menu.add(R.string.usi_menu_settings).setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(MenuItem item) {
-                startActivity(new Intent(android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS, Uri.parse("package:" + getPackageName())));
+                startActivity(new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS, Uri.parse("package:" + getPackageName())));
                 return true;
             }
         });
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+            settingsItem.setShowAsActionFlags(MenuItem.SHOW_AS_ACTION_NEVER);
+        }
         return true;
     }
 
