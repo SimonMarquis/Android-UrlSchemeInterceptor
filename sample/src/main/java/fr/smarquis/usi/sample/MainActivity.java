0ead6e1aa334dfc0c9374b97c6f2cbebaba470bd
Simon Marquis <contact@simon-marquis.fr>
2022-10-09 20:26:48 +0200

    Get ready for target API level requirements (API 33)

--- sample/src/main/java/fr/smarquis/usi/sample/MainActivity.java
+++ sample/src/main/java/fr/smarquis/usi/sample/MainActivity.java
@@ -4,8 +4,9 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.view.View;
+
+import androidx.annotation.NonNull;
 
 import java.util.Date;
 
--- usi/src/main/java/fr/smarquis/usi/InterceptorActivity.java
+++ usi/src/main/java/fr/smarquis/usi/InterceptorActivity.java
@@ -7,11 +7,12 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 public class InterceptorActivity extends Activity {
 
